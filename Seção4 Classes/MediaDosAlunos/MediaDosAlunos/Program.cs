dacf5fc55feba49158d17da711ab106ebd46e1da
AndréPousa <andre.roberto.pousa@gmail.com>
2021-05-16 19:32:40 -0300

    Correção dos exercicios da seção4

--- Seção4 Classes/MediaDosAlunos/MediaDosAlunos/Program.cs
+++ Seção4 Classes/MediaDosAlunos/MediaDosAlunos/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace MediaDosAlunos
 {
@@ -6,8 +7,28 @@ namespace MediaDosAlunos
     {
         static void Main(string[] args)
         {
-            Console.Write("Digite o nome do aluno");
+            Alunos aluno = new Alunos();
 
+            Console.Write("Digite o Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("NOTA FINAL = ");
+            aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture);
+            if (aluno.Aprovado())
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM "
+                + aluno.NotaRestante().ToString("F2", CultureInfo.InvariantCulture)
+                + " PONTOS");
+            }
         }
     }
 }
+
