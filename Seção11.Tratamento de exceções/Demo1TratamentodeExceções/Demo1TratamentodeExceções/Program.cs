87701457a99fcbabebff3c361e95d17a44f5219c
AndréPousa <andre.roberto.pousa@gmail.com>
2021-05-26 17:41:55 -0300

    Ex tratamento de exceções

---
+++ Seção11.Tratamento de exceções/Demo1TratamentodeExceções/Demo1TratamentodeExceções/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Demo1TratamentodeExceções
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+
+                Console.WriteLine(result);
+
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Não é permitido divisão por 0");
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(" Formato errado ! " + e.Message);
+            }
+        }
+    }
+}
