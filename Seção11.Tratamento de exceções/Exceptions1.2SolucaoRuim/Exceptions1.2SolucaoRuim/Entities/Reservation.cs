87f8e25350e50585d9843cd5d29cbac03072041b
AndréPousa <andre.roberto.pousa@gmail.com>
2021-05-27 17:21:07 -0300

    Finalizados os exercicios de tratamento de exceção

---
+++ Seção11.Tratamento de exceções/Exceptions1.2SolucaoRuim/Exceptions1.2SolucaoRuim/Entities/Reservation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exceptions1._2SolucaoRuim.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+
+        }
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        //metodo para atualizar as datas
+        public string UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                return "Reservation dates for update must be future dates";
+            }
+
+            if (checkOut <= checkIn)
+            {
+                return "CheckOut date must be after check-in date ";
+            }
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return "Romm "
+                + RoomNumber
+                + ", checkin-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights ";
+        }
+    }
+}
+
+
---
+++ Seção11.Tratamento de exceções/Exceptions1.2SolucaoRuim/Exceptions1.2SolucaoRuim/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Exceptions1._2SolucaoRuim.Entities;
+
+namespace Exceptions1._2SolucaoRuim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Room Number; ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Checkin-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Checkin-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Error in Reservation: CheckOut date must be after check-in date ");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Checkin-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Checkin-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                string error = reservation.UpdateDates(checkIn, checkOut);
+
+                if(error != null)
+                {
+                    Console.WriteLine("Erro in reservation: " + error);
+                }
+
+                else
+                {                 
+                    Console.WriteLine("Reservation: " + reservation);
+                }
+            }
+        }
+    }
+}
+
