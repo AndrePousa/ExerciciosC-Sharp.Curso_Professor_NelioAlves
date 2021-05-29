6fd8ab5fb7a7c63bfca4efd112a0514bfd7a99f9
AndréPousa <andre.roberto.pousa@gmail.com>
2021-05-29 18:06:10 -0300

    finalizados os exercicios

---
+++ Secão7.Tópicos especiais1/CondicionalTernaria/CondicionalTernaria/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace CondicionalTernaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Opcional ao if-else quando se deseja decidir um valor com base em uma condição
+
+            /* Sintaxe:
+               (condição) ? valor_se_verdadeiro : valor_se_falso
+
+            Exemplos:
+               (2 > 4) ? 50 : 80 ---------> 80
+
+               (10 != 3) ? "Maria" : "Alex" ----------->  "Maria" */
+
+            Console.WriteLine("Digite o preço");
+
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+            /* if( preco < 20.0)
+            {
+                desconto = preco * 0.1;
+            }
+            else
+            {
+                desconto = preco * 0.05;
+            } */
+
+            Console.WriteLine(desconto);
+        }
+    }
+}
---
+++ Secão7.Tópicos especiais1/switch-case/switch-case/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número de 1 a 7 ");
+            int x = int.Parse(Console.ReadLine());
+            string day;
+
+            switch (x)
+            {
+                case 1:
+                    day = "domingo";
+                    break;
+                case 2:
+                    day = "segunda";
+                    break;
+                case 3:
+                    day = "terça";
+                    break;
+                case 4:
+                    day = "quarta";
+                    break;
+                case 5:
+                    day = "quinta";
+                    break;
+                case 6:
+                    day = "sexta";
+                    break;
+                case 7:
+                    day = "sabado";
+                    break;
+                default:
+                    day = "valor invalido";
+                    break;
+            }
+             Console.WriteLine(day);
+        }
+    }
+}
